9de03cdc54c552b7eda4d3cb49de254c98d0a27e
Mblanco24 <marilynblanco1998@gmail.com>
2024-10-18 01:12:31 -0400

    FIN

--- database/seeders/CarsTableSeeder.php
+++ database/seeders/CarsTableSeeder.php
@@ -4,14 +4,38 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Car;
 
 class CarsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Car::create([
+            'marca' => 'Ford',
+            'modelo' => 'Shelby',
+            'año' => '2022',
+            'color' => 'Azul',
+            'precio' => '2000000',
+            'kilometraje' => '100000',
+        ]);
+        Car::create([
+            'marca' => 'Toyota',
+            'modelo' => 'Shelby',
+            'año' => '2022',
+            'color' => 'Azul',
+            'precio' => '2000000',
+            'kilometraje' => '100000',
+        ]);
+        Car::create([
+            'marca' => 'Ford',
+            'modelo' => 'Shelby',
+            'año' => '2022',
+            'color' => 'Azul',
+            'precio' => '2000000',
+            'kilometraje' => '100000',
+        ]);
     }
 }
